df490eeec9ff4e209f4588be9869b3e19af87776
DFDesktop209 <kylealino@gmail.com>
2025-10-23 17:09:29 +0800

    login ui fix

--- app/Views/MyLogin.php
+++ app/Views/MyLogin.php
@@ -26,9 +26,9 @@
       <div class="position-relative z-index-5">
         <div class="row">
           <div class="col-xl-7 col-xxl-8">
-            <a href="main/index.html" class="text-nowrap logo-img d-block px-4 py-9 w-100">
-              <img src="<?=base_url('assets/images/logos/fnrilogo.png')?>" class="dark-logo" alt="Logo-Dark" style="width: 70px; height: auto;" />
-              <img src="<?=base_url('assets/images/logos/fnrilogo.png')?>" class="light-logo" alt="Logo-light" style="width: 70px; height: auto;" />
+            <a href="" class="text-nowrap logo-img d-block px-4 py-9 w-100">
+              <img src="<?=base_url('assets/images/logos/fnrilogo.png')?>" class="dark-logo" alt="Logo-Dark" style="width: 40px; height: auto;" />
+              <img src="<?=base_url('assets/images/logos/fnrilogo.png')?>" class="light-logo" alt="Logo-light" style="width: 40px; height: auto;" />
             </a>
             <div class="d-none d-xl-flex align-items-center justify-content-center h-n80">
               <img src="<?=base_url('assets/images/backgrounds/login-security.svg')?>" alt="flexy-img" class="img-fluid" width="500">
@@ -37,28 +37,14 @@
           <div class="col-xl-5 col-xxl-4">
             <div class="authentication-login min-vh-100 bg-body row justify-content-center align-items-center p-4">
               <div class="auth-max-width col-sm-8 col-md-6 col-xl-7 px-4">
-                <h2 class="mb-1 fs-7 fw-bolder text-center">FODS</h2>
-                <p class="mb-7 text-center">Financial Obligation and Disbursement System</p>
-                <div class="row">
-                  <div class="col-6 mb-2 mb-sm-0">
-                    <a class="btn text-dark border fw-normal d-flex align-items-center justify-content-center rounded-2 py-8" href="javascript:void(0)" role="button">
-                      <img src="<?=base_url('assets/images/svgs/google-icon.svg')?>" alt="flexy-img" class="img-fluid me-2" width="18" height="18">
-                      <span class="flex-shrink-0">with Google</span>
-                    </a>
-                  </div>
-                  <div class="col-6">
-                    <a class="btn text-dark border fw-normal d-flex align-items-center justify-content-center rounded-2 py-8" href="javascript:void(0)" role="button">
-                      <img src="<?=base_url('assets/images/svgs/facebook-icon.svg')?>" alt="flexy-img" class="img-fluid me-2" width="18" height="18">
-                      <span class="flex-shrink-0">with FB</span>
-                    </a>
-                  </div>
-                </div>
-                <div class="position-relative text-center my-4">
-                  <p class="mb-0 fs-4 px-3 d-inline-block bg-body text-dark z-index-5 position-relative">or sign
-                    in
-                    with</p>
-                  <span class="border-top w-100 position-absolute top-50 start-50 translate-middle"></span>
-                </div>
+                
+                <a href="" class="text-nowrap logo-img d-block px-4 py-9 w-100 text-center">
+                  <img src="<?=base_url('assets/images/logos/fnrilogo.png')?>" class="dark-logo" alt="Logo-Dark" style="width: 70px; height: auto;" />
+                  <img src="<?=base_url('assets/images/logos/fnrilogo.png')?>" class="light-logo" alt="Logo-light" style="width: 70px; height: auto;" />
+                </a>
+                <h2 class="mb-1 fs-10 fw-bolder text-center text-info">F O D S</h2>
+                <p class="mb-7 text-center fw-bolder">Financial Obligation and Disbursement System</p>
+
                 <form action="<?=site_url();?>mylogin-auth" method="post" novalidate>
                   <div class="mb-3">
                     <label for="MyUsername" class="form-label">Username</label>
@@ -72,10 +58,10 @@
                     <div class="form-check">
                       <input class="form-check-input primary" type="checkbox" value="" id="flexCheckChecked" checked>
                       <label class="form-check-label text-dark fs-3" for="flexCheckChecked">
-                        Remeber this Device
+                        Remember this Device
                       </label>
                     </div>
-                    <a class="text-primary fw-medium fs-3" href="">Forgot Password ?</a>
+                    <a class="text-primary fw-medium fs-3" href="">Forgot Password?</a>
                   </div>
                   <!-- <a href="../main/index.html" class="btn btn-primary w-100 py-8 mb-4 rounded-2">Sign In</a> -->
                   <button type="submit" class="btn btn-primary w-100 mb-4 rounded-2">Sign In</button>
@@ -83,14 +69,13 @@
                     $cmesslogin = session()->getFlashdata('mesyszicas_memsg_login');
                     if(!empty($cmesslogin)):
                       echo "<div class=\"col-12 d-flex align-items-center justify-content-center mb-3\">
-                        <div style=\"color: #B81111\">{$cmesslogin}</div>
+                        <div style=\"color: #B81111\">{$cmesslogin}!</div>
                       </div>";
                     endif;
                   ?>    
                   <div class="d-flex align-items-center justify-content-center">
-                    <p class="fs-4 mb-0 fw-medium">New to spike?</p>
-                    <a class="text-primary fw-medium ms-2" href="">Create an
-                      account</a>
+                    <p class="fs-4 mb-0 fw-medium">New to FODS?</p>
+                    <a class="text-primary fw-medium ms-2" href="">Contact the administrator</a>
                   </div>
                 </form>
               </div>
